e776e4554cfeeabf36523ed73a6302d0afa71113
graduses <grad@gradusess-iMac.local>
2020-01-25 14:28:22 +0200

    Finished implementing realtime search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
-import './App.css';
+
+import dummyImg from './frame.png';
+
+import {
+  Container,
+  Input,
+  Image,
+  List,
+  Transition,
+  Sticky,
+  Popup,
+  Placeholder,
+  Divider
+} from 'semantic-ui-react';
 
 export default class App extends React.Component {
   constructor() {
@@ -7,36 +20,103 @@ export default class App extends React.Component {
 
     this.state = {
       contacts: [],
-      search: ''
+      search: '',
+      loading: true
     };
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(res => this.setState({ contacts: res }));
+      .then(res => this.setState({ contacts: res, loading: false }));
   }
 
   render() {
-    const shownContacts = this.state.contacts;
+    const shownContacts = this.state.contacts.filter(elem => {
+      const lc = elem.name.toLowerCase();
+      const filter = this.state.search.toLowerCase();
+
+      return lc.includes(filter);
+    });
+
+    const placeholders = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     return (
-      <div>
-        <input
-          type="text"
-          placeholder="Search..."
-          onChange={e => this.setState({ search: e.target.value })}
-          value={this.state.search}
-        />
-        <ul>
-          {shownContacts.map(elem => (
-            <li key={elem.id}>
-              <h4>{elem.name}</h4>
-              <span>{elem.phone}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Container>
+        <Sticky>
+          <Popup
+            trigger={
+              <Input
+                placeholder="Search..."
+                className="input-search"
+                icon="search"
+                fluid
+                onChange={e =>
+                  this.setState({ search: e.target.value.trimStart() })
+                }
+                value={this.state.search}
+              />
+            }
+            content="Search for your contacts"
+          />
+        </Sticky>
+
+        {
+          <Transition.Group
+            as={List}
+            animation="fade right"
+            duration={200}
+            divided
+          >
+            {this.state.loading
+              ? placeholders.map(elem => (
+                  <div key={elem}>
+                    <Placeholder>
+                      <Placeholder.Header image>
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                      </Placeholder.Header>
+                    </Placeholder>
+                    <Divider />
+                  </div>
+                ))
+              : shownContacts.map(elem => {
+                  const parts = elem.name.split(
+                    new RegExp(`(${this.state.search})`, 'gi')
+                  );
+
+                  return (
+                    <List.Item key={elem.id}>
+                      <Image src={dummyImg} avatar />
+                      <List.Content>
+                        <List.Header as="h4">
+                          {parts.map((part, i) =>
+                            part.toLowerCase() ===
+                              this.state.search.toLowerCase() &&
+                            this.state.search.length > 0 ? (
+                              <span
+                                key={i}
+                                style={{
+                                  borderRadius: '3px',
+                                  background: '#2185d0',
+                                  color: 'white'
+                                }}
+                              >
+                                {part}
+                              </span>
+                            ) : (
+                              <span>{part}</span>
+                            )
+                          )}
+                        </List.Header>
+                        <List.Description>{elem.phone}</List.Description>
+                      </List.Content>
+                    </List.Item>
+                  );
+                })}
+          </Transition.Group>
+        }
+      </Container>
     );
   }
 }
